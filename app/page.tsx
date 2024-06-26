c4c6db4c36ac29b06f510969e5fab2661742776d
anand06amar <anand07amar@gmail.com>
2024-06-26 23:59:08 +0530

    added Experience part and approach part

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
@@ -16,6 +17,7 @@ const Home = () => {
         <Grid />
         <RecentProjects />
         <Experience />
+        <Approach/>
       
 
         
